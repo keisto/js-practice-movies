b87e08e493cb471ee9024e5f57d562fb290ca1ff
Tony Keiser <keisertony@gmail.com>
2023-01-21 13:50:03 -0800

    Created debounce utility function

---
+++ utils.js
@@ -0,0 +1,12 @@
+const debounce = (fn, delay = 1000) => {
+  let timeoutId
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    timeoutId = setTimeout(() => {
+      fn.apply(null, args)
+    }, delay)
+  }
+}
